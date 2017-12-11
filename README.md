cc10ee2d5a633ba099b143dcbcb33eb5e9330784
Paweł <pawel.cmachowski@gmail.com>
2017-12-11 15:36:49 +0100

    Read me seed

---
+++ README.md
@@ -0,0 +1,11 @@
+# Available TASKS
+
+# wordpress docker start
+# wordpress docker stop
+
+# wordpress db import <filename:optional, default: last exported>
+# wordpress db export
+
+# wordpress init
+# wordpress frontend
+# wordpress run
--- wordpress.sh
+++ wordpress.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Available TASKS
-
-# wordpress docker start
-# wordpress docker stop
-
-# wordpress db import <filename:optional, default: last exported>
-# wordpress db export
-
-# wordpress init
-# wordpress frontend
-# wordpress run
-
 BASH_GROUP='pawel'
 BASH_USER='pawel'
 
