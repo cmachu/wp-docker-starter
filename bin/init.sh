5b73011f26de60f5218c80c5596af350db8894b4
Paweł Ćmachowski <pawel.cmachowski@polcode.net>
2018-01-26 10:52:49 +0100

    Change init procedure

--- bin/init.sh
+++ bin/init.sh
@@ -24,7 +24,7 @@ docker-compose exec web sh -c "exec wp --allow-root config create --dbname=$8 --
 docker-compose exec web sh -c "exec wp --allow-root core multisite-install --admin_user=$3 --admin_password=$4 --admin_email=$5 --url=$6 --title='$7'";
 
 
-docker-compose exec web sh -c "exec mkdir /app/wp-content/uploads"
+#docker-compose exec web sh -c "exec mkdir /app/wp-content/uploads"
 docker-compose exec web sh -c "exec chmod -R 777 /app"
 docker-compose exec web sh -c "exec chown -R www-data:www-data /app"
 
